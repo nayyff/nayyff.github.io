fb0e55065719feeec363b1b92fb30d9f12987d6b
nayyff <rasyikh2008@gmail.com>
2024-11-17 21:57:36 +0700

    update email.js

--- email.js
+++ email.js
@@ -1,8 +1,6 @@
 function sendmail(){
     let parms={
         name : document.getElementById("name").value,
-        contact_number : document.getElementById("contact_number").value,
-        gender : document.getElementById("gender").value,
         email : document.getElementById("email").value,
     }
 
