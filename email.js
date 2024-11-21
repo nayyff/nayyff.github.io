146fc72b255a9cf47bfc8ad7db9109b306860d37
nayyff <rasyikh2008@gmail.com>
2024-11-21 14:55:28 +0700

    nambah kofig email js

--- email.js
+++ email.js
@@ -1,8 +1,17 @@
+// function sendmail(){
+//     let parms={
+//         name : document.getElementById("name").value,
+//         email : document.getElementById("email").value,
+//     }
+
+//     emailjs.send("service_mbvt21f","template_up9540b",parms)
+// }
+
 function sendmail(){
     let parms={
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
     }
 
-    emailjs.send("service_mbvt21f","template_up9540b",parms)
+    emailjs.send("service_ej31pff","template_ijlsp0r",parms)
 }
