00833818d95e0dbc9beb24d141ddc9bc68b3201d
nayyff <rasyikh2008@gmail.com>
2024-11-22 23:38:09 +0700

    added emailjs config

--- email.js
+++ email.js
@@ -7,6 +7,7 @@
 //     emailjs.send("service_mbvt21f","template_up9540b",parms)
 // }
 
+// email trial
 function sendmail(){
     let parms={
         name : document.getElementById("name").value,
@@ -14,4 +15,141 @@ function sendmail(){
     }
 
     emailjs.send("service_rj4f4am","template_ijlsp0r",parms)
+}
+// email ekor
+
+// 1
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_cp9a8vk","template_cxxhwhf",parms)
+}
+// 2
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_47yv3ru","template_59eyxxb",parms)
+}
+// 3
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_tr4jj04","template_rlk8vbb",parms)
+}
+// 4
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_83yxuqw","template_eh36j4b",parms)
+}
+// 5
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_6tdpjqb","template_bjy2b6b",parms)
+}
+// 6
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_irlp9sl","template_nk39k0r",parms)
+}
+// 7
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_4euqcg9","template_jmuy8yr",parms)
+}
+// 8
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_1r6jgqm","template_lnnwudr",parms)
+}
+// 9
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_smaff5g","template_6b5yr4pr",parms)
+}
+// 10
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_nah9eqo","template_ymxjfz7",parms)
+}
+// 11
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_mi40ygj","template_hg8ci7h",parms)
+}
+// 12
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_ra2ns9v","template_c3pu3as",parms)
+}
+// 13
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_wvkm7du","template_dff3c2p",parms)
+}
+// 14
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_oq60f9v","template_qykiwzb",parms)
+}
+// 15
+function sendmail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+    }
+
+    emailjs.send("service_j2vhosh","template_7betx9g",parms)
 }
